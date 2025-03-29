8c461ef30d25c3f82fa798a8515ccb8bce033ede
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-03-29 21:31:00 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ MySQL/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,17 +1,19 @@
-# 240924 TUE PM 6:53
-# (from) MEMBER_PROFILE 테이블에서
-# (where) 생일이 3월인 여성 회원의 ID, 이름, 성별, 생년월일을 조회하는 SQL문을 작성해주세요.
-# (where) 이때 전화번호가 NULL인 경우는 출력대상에서 제외시켜 주시고,
-# (order by) 결과는 회원ID를 기준으로 오름차순 정렬해주세요.
+# 250329 토 PM 9:30
+/*
+(FROM) MEMBER_PROFILE 테이블에서
+(SELECT, WHERE) 생일이 3월인 여성 회원의 ID, 이름, 성별, 생년월일을 조회하는 SQL문을 작성해주세요.
+(WHERE) 이때 전화번호가 NULL인 경우는 출력대상에서 제외시켜 주시고,
+(ORDER BY) 결과는 회원ID를 기준으로 오름차순 정렬해주세요.
+*/
 
 SELECT
     member_id,
     member_name,
     gender,
-    date_format(date_of_birth, '%Y-%m-%d') as date_of_birth
-FROM member_profile
+    date_format(date_of_birth, '%Y-%m-%d') as DATE_OF_BIRTH
+FROM MEMBER_PROFILE
 WHERE
-    tlno is not NULL AND
+    date_of_birth LIKE '%-03-%' AND
     gender = 'W' AND
-    date_of_birth LIKE '%-03-%'
+    tlno is not NULL
 ORDER BY member_id
