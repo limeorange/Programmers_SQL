0a447b0b1b7f926bdd221d658ec21b9ece4ceb00
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-04-02 17:33:52 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ MySQL/프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,26 +1,21 @@
-# 240924 TUE PM 7:19
-# REST_INFO와 REST_REVIEW 테이블에서 
-# 서울에 위치한 식당들의
-# 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수를 조회하는 SQL문을 작성해주세요.
-# 이때 리뷰 평균점수는 소수점 세 번째 자리에서 반올림 해주시고
-# 결과는 평균점수를 기준으로 내림차순 정렬해주시고, 평균점수가 같다면 즐겨찾기수를 기준으로 내림차순 정렬해주세요.
+# 250402 수 PM 5:32
+/*
+오답포인트) GROUP BY i.REST_ID 만 사용
+=> 정확한 값을 보장하려면, GROUP BY 대상에 모든 컬럼을 넣거나,
+서브쿼리로 따로 SCORE만 집계해서 JOIN하는 방식을 활용해야 함
+*/
 
 SELECT
-    i.rest_id,
-    rest_name,
-    food_type,
-    favorites,
-    address,
-    round(avg(review_score), 2) AS SCORE
-FROM rest_info as i
-JOIN rest_review as r
-ON i.rest_id = r.rest_id
-WHERE
-    i.address LIKE '서울%'
+    i.REST_ID,
+    REST_NAME,
+    FOOD_TYPE,
+    FAVORITES,
+    ADDRESS,
+    ROUND(AVG(r.REVIEW_SCORE), 2) AS SCORE
+FROM REST_INFO as i
+JOIN REST_REVIEW as r
+ON i.REST_ID = r.REST_ID
+WHERE ADDRESS LIKE '서울%'
 GROUP BY
-    i.rest_id,
-    rest_name,
-    food_type,
-    favorites,
-    address
+    i.REST_ID
 ORDER BY SCORE DESC, FAVORITES DESC
