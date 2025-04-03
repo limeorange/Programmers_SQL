d7b5204c7c1ae2b4725266498d5ac28872a3cfe3
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-04-03 20:33:41 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ MySQL/프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,19 +1,13 @@
-# 240927 금 AM 2:24
+# 250403 목 PM 8:33
 
-# 천재지변으로 인해 일부 데이터가 유실되었습니다.
-# 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
-
-# WHERE 절에서 서브쿼리 활용해서 NOT IN으로 원하는 조건('보호소에 들어온 기록이 없는') 설정
+WITH
+    not_list AS (
+    SELECT ANIMAL_ID
+    FROM ANIMAL_INS
+    )
 
 SELECT
     ANIMAL_ID,
     NAME
 FROM ANIMAL_OUTS
-WHERE
-    ANIMAL_ID NOT IN (
-        SELECT
-            ANIMAL_ID
-        FROM
-            ANIMAL_INS
-    )
-ORDER BY ANIMAL_ID
+WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID FROM not_list)
