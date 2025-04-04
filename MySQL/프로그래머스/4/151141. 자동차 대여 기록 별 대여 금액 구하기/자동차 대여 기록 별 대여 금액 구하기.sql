412abbc3bf895af5827ba626e0419cb86ad3ed23
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-04-04 21:05:49 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,37 +1,48 @@
-# 240928 토 AM 1:35
+# 250404 금 PM 9:05
+# 오답포인트) 날짜 차이 계산시 '-' 사용하면 부정확 (단순 숫자 계산)
+# => DATEDIFF(end_date, start_date) 활용!
 
-# 자동차 종류가 '트럭'인 자동차의 대여 기록에 대해서 
-# 대여 기록 별로 대여 금액(컬럼명: FEE)을 구하여 
-# 대여 기록 ID와 대여 금액 리스트를 출력하는 SQL문을 작성해주세요.
-# 결과는 대여 금액을 기준으로 내림차순 정렬하고, 
-# 대여 금액이 같은 경우 대여 기록 ID를 기준으로 내림차순 정렬해주세요.
-
-# 1. 특정 열의 null 값 처리 => COALESCE(expr1, expr2, ...) 사용
-# 2. 날짜 간 차이는 '-' 사용 불가 !! => DATEDIFF(end_date, start_date) 사용
+WITH 
+    his_table AS (
+    SELECT
+        history_id,
+        h.car_id,
+        daily_fee,
+        car_type,
+        DATEDIFF(end_date, start_date)+1 AS days
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
+    JOIN CAR_RENTAL_COMPANY_CAR as c
+    ON h.car_id = c.car_id
+    WHERE car_type = '트럭'
+    ),
+    
+    discount AS (
+    SELECT
+        duration_type,
+        discount_rate
+    FROM car_rental_company_discount_plan
+    WHERE car_type = '트럭'
+    ),
+    
+    fee_table AS (
+    SELECT
+        history_id,
+        daily_fee,
+        days,
+        CASE
+            WHEN (7 <= days AND days < 30) THEN 
+            (SELECT discount_rate FROM discount WHERE duration_type = '7일 이상')
+            WHEN (30 <= days AND days < 90) THEN 
+            (SELECT discount_rate FROM discount WHERE duration_type = '30일 이상')
+            WHEN (90 <= days) THEN 
+            (SELECT discount_rate FROM discount WHERE duration_type = '90일 이상')
+            ELSE 0
+        END AS discount
+    FROM his_table
+    )
 
 SELECT
     HISTORY_ID,
-    round(sum(daily_fee*(1-0.01*COALESCE(discount_rate, 0))*diff)) AS FEE
-FROM
-    (SELECT
-        h.HISTORY_ID,
-        h.CAR_ID,
-        start_date,
-        end_date,
-        DATEDIFF(end_date, start_date) + 1 AS diff,
-        CASE
-            WHEN DATEDIFF(end_date, start_date) + 1 >= 90 THEN "90일 이상"
-            WHEN DATEDIFF(end_date, start_date) + 1 >= 30 THEN "30일 이상"
-            WHEN DATEDIFF(end_date, start_date) + 1 >= 7 THEN "7일 이상"
-        END AS DURATION_TYPE,
-        c.car_type,
-        daily_fee
-    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY AS h
-    JOIN CAR_RENTAL_COMPANY_CAR AS c
-    ON h.CAR_ID = c.CAR_ID
-    WHERE c.car_type = '트럭'
-    ) AS t
-LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS p
-ON t.DURATION_TYPE = p.DURATION_TYPE and t.CAR_TYPE = p.CAR_TYPE
-GROUP BY HISTORY_ID
-ORDER BY FEE DESC, HISTORY_ID DESC
+    ROUND(daily_fee * days * (1-0.01*discount)) AS FEE
+FROM fee_table
+ORDER BY fee DESC, history_id DESC
