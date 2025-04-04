034c8badfff98fbba4029ff78d06cbe63516e014
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-04-04 21:45:09 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ MySQL/프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,20 +1,16 @@
-# 240929 일 PM 11:51
+# 250404 금 PM 9:45
 
-# USED_GOODS_BOARD와 USED_GOODS_FILE 테이블에서
-# 조회수가 가장 높은 중고거래 게시물에 대한 첨부파일 경로를 조회하는 SQL문을 작성해주세요.
-# 첨부파일 경로는 FILE ID를 기준으로 내림차순 정렬해주세요. 
-# 기본적인 파일경로는 /home/grep/src/ 이며, 
-# 게시글 ID를 기준으로 디렉토리가 구분되고, 파일이름은 파일 ID, 파일 이름, 파일 확장자로 구성되도록 출력해주세요. 
-# 조회수가 가장 높은 게시물은 하나만 존재합니다.
+WITH top_board_id AS (
+    SELECT
+        BOARD_ID
+    FROM USED_GOODS_BOARD
+    ORDER BY VIEWS DESC
+    LIMIT 1
+)
 
 SELECT
-    concat('/home/grep/src/', F.BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
-FROM USED_GOODS_FILE AS F
-JOIN USED_GOODS_BOARD AS B
-ON F.board_id = B.board_id
-WHERE VIEWS = (
-    SELECT
-        MAX(VIEWS)
-    FROM USED_GOODS_BOARD
-)
-ORDER BY FILE_ID DESC
+    CONCAT('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) AS file_path
+FROM USED_GOODS_FILE
+WHERE
+    board_id in (SELECT board_id FROM top_board_id)
+ORDER BY file_id DESC
