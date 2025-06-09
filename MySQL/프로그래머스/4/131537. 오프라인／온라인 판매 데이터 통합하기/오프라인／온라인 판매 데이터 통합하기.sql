9a0f0964232b39f44dd56a0e76c25ea119dd6fc2
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-06-09 23:31:45 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ MySQL/프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,25 +1,26 @@
-# 250402 수 PM 6:01
-/*
-오답포인트) offline_sale 테이블에서 user_id 컬럼 만들 때 'NULL'이 아닌 NULL로 명시
-*/
+# 250609 월 PM 11:31
 
-WITH all_sale AS (
-    SELECT sales_date, product_id, user_id, sales_amount
-    FROM ONLINE_SALE
+WITH all_sales AS (
+    SELECT
+        sales_date,
+        product_id,
+        user_id,
+        sales_amount
+    FROM online_sale
     UNION
-    SELECT sales_date, product_id, NULL AS user_id, sales_amount
-    FROM OFFLINE_SALE
+    SELECT
+        sales_date,
+        product_id,
+        NULL,
+        sales_amount
+    FROM offline_sale
 )
 
 SELECT
-    DATE_FORMAT(sales_date, '%Y-%m-%d') AS SALES_DATE,
-    PRODUCT_ID,
-    USER_ID,
-    SUM(SALES_AMOUNT) AS SALES_AMOUNT
-FROM all_sale
-WHERE SALES_DATE LIKE '2022-03%'
-GROUP BY
-    SALES_DATE,
-    PRODUCT_ID,
-    USER_ID
-ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
+    DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date,
+    product_id,
+    user_id,
+    sales_amount
+FROM all_sales
+WHERE YEAR(sales_date) = 2022 AND MONTH(sales_date) = 3
+ORDER BY sales_date, product_id, user_id
