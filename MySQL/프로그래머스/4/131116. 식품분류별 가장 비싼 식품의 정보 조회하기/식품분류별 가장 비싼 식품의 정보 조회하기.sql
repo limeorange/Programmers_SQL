1046b941256185c9b4e408779d6422046fe7a4dc
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-06-10 17:14:41 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ MySQL/프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,20 +1,22 @@
-# 250402 수 PM 9:57
+# 250610 화 PM 5:14
 
-WITH max_table AS (
+/*
+(카테고리, 최고가) 정보를 CTE로 먼저 구한 뒤, WHERE 조건으로 식품 정보(product_name)를 추출해야 함.
+처음에 GROUP BY로 카테고리별 최고가 정보 구할 때 SELECT 문에 product_name 적어줘도
+이는 최고가에 대한 product_name이 아닐 수 있음.
+*/
+
+WITH food_info AS (
     SELECT
-    category,
-    MAX(price) AS max_price
-    FROM FOOD_PRODUCT
-    WHERE
-        CATEGORY IN ('과자', '국', '김치', '식용유')
+        category,
+        MAX(price) AS max_price
+    FROM food_product
     GROUP BY category
+    HAVING category in ('과자', '국', '김치', '식용유')
 )
 
 SELECT
-    category,
-    price,
-    product_name
-FROM FOOD_PRODUCT
-WHERE
-    (category, price) in (SELECT category, max_price FROM max_table)
-ORDER BY price DESC
+    category, price AS max_price, product_name
+FROM food_product
+WHERE (category, price) in (SELECT category, max_price FROM food_info)
+ORDER BY max_price DESC
