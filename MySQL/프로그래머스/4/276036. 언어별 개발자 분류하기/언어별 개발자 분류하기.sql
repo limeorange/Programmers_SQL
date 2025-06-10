1bd96859ed7f1047c213e32bea58ef5a554a6ed5
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-06-10 20:47:17 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ MySQL/프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,44 +1,53 @@
-# 250403 목 PM 3:33
+# 250610 화 PM 8:47
 
 /*
-오답포인트) A, B, C 테이블을 각각 구해서 UNION하는 방법은 중복 처리가 제대로 안됨
-1) A, B, C 등급 조건 처리 (CASE WHEN)
-2) WHERE 조건으로 A, B, C 등급에 해당하지 않는 개발자 제외하기
-- not null 조건 주려면 서브쿼리나 CTE로 작성해야 함
+1. A -> B -> C 순으로 개발자 등급 정보 구해서 합치기
+- Front End 스킬코드 합 구하기 (A grade 구할 때 사용)
 */
 
 WITH
-    F AS (
-    SELECT SUM(CODE) AS CODE
-    FROM SKILLCODES
-    WHERE CATEGORY = 'Front End'
-    ),
-    C AS (
-    SELECT CODE AS CODE
-    FROM SKILLCODES
-    WHERE NAME = 'C#'
-    ),
-    P AS (
-    SELECT SUM(CODE) AS CODE
-    FROM SKILLCODES
-    WHERE NAME = 'Python'
-    ),
-    dev_table AS (
+    front_code AS (
     SELECT
-    CASE
-        WHEN SKILL_CODE & (SELECT CODE FROM F) > 0 AND
-             SKILL_CODE & (SELECT CODE FROM P) > 0 THEN 'A'
-        WHEN SKILL_CODE & (SELECT CODE FROM C) > 0 THEN 'B'
-        WHEN SKILL_CODE & (SELECT CODE FROM F) > 0 AND
-             SKILL_CODE & (SELECT CODE FROM P) = 0 THEN 'C'
-    END AS GRADE,
-    ID,
-    EMAIL
-    FROM DEVELOPERS
+        SUM(code) AS code
+    FROM skillcodes
+    WHERE category = 'Front End'
+    ),
+    
+    a_grade AS (
+    SELECT
+        *,
+        'A' AS grade
+    FROM developers
+    WHERE skill_code & (SELECT code FROM front_code) and
+          skill_code & (SELECT code FROM skillcodes WHERE name = 'Python')
+    ),
+    
+    b_grade AS (
+    SELECT
+        *,
+        'B' AS grade
+    FROM developers
+    WHERE id not in (SELECT id FROM a_grade) and
+          skill_code & (SELECT code FROM skillcodes WHERE name = 'C#')
+    ),
+    
+    c_grade AS (
+    SELECT
+        *,
+        'C' AS grade
+    FROM developers
+    WHERE id not in (SELECT id FROM a_grade) and
+          id not in (SELECT id FROM b_grade) and
+          skill_code & (SELECT code FROM front_code)
+    ),
+    
+    union_table AS (
+    SELECT * FROM a_grade UNION
+    SELECT * FROM b_grade UNION
+    SELECT * FROM c_grade
     )
-
+    
 SELECT
-    *
-FROM dev_table
-WHERE GRADE is not NULL
-ORDER BY GRADE, ID
+    grade, id, email
+FROM union_table
+ORDER BY grade, id
