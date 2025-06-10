013f0f30f72bf15a49fd8840712f0a78b1fcdf91
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-06-10 13:46:59 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ MySQL/프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,28 +1,29 @@
-# 250402 수 PM 6:14
+# 250610 화 PM 1:46
+
 /*
 - g1: 1세대 대장균 ID (1, 2) => 부모 ID가 NULL
-- g2: 2세대 대장균 ID (3, 4, 5) => 부모 ID가 1, 2
-- g3: 3세대 대장균 ID (6, 7) => 부모 ID가 3, 4, 5
+- g2: 2세대 대장균 ID (3, 4, 5) => 부모 ID가 1, 2 (SELECT ID FROM g1)
+- g3: 3세대 대장균 ID (6, 7) => 부모 ID가 3, 4, 5 (SELECT ID FROM g2)
 */
 
-WITH 
+WITH
     g1 AS (
     SELECT ID
-    FROM ECOLI_DATA
-    WHERE PARENT_ID is NULL
+    FROM ecoli_data
+    WHERE parent_id is NULL
     ),
     g2 AS (
     SELECT ID
-    FROM ECOLI_DATA
-    WHERE PARENT_ID IN (SELECT ID FROM g1)
+    FROM ecoli_data
+    WHERE parent_id in (SELECT ID FROM g1)
     ),
     g3 AS (
     SELECT ID
-    FROM ECOLI_DATA
-    WHERE PARENT_ID IN (SELECT ID FROM g2)
+    FROM ecoli_data
+    WHERE parent_id in (SELECT ID FROM g2)
     )
 
 SELECT
-    ID
+    *
 FROM g3
 ORDER BY ID
