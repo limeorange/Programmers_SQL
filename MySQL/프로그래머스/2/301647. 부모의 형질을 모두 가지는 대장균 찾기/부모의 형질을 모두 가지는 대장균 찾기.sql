6f35a5d2840431ed49863e2058c92e87fb1379c2
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-06-10 16:41:01 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ MySQL/프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -1,17 +1,12 @@
-# 250330 일 AM 12:11
-/*
-조건) 아기 대장균이 부모의 형질을 모두 가지고 있어야 함
-=> 한 숫자(GENOTYPE)에 여러 형질(유전자 정보)이 들어있음
-=> 하나의 숫자에 여러 형질이 비트 단위로 압축되어 있는 것!
-=> A & B = A와 B의 비트 단위 AND 연산 활용
-*/
+# 250610 화 PM 4:40
 
 SELECT
-    child.ID,
-    child.GENOTYPE,
-    parent.GENOTYPE AS PARENT_GENOTYPE
-FROM ECOLI_DATA AS child
-JOIN ECOLI_DATA AS parent
+    child.id,
+    child.genotype,
+    parent.genotype AS parent_genotype
+FROM ecoli_data AS child
+JOIN ecoli_data AS parent
 ON child.parent_id = parent.id
-WHERE child.GENOTYPE & parent.GENOTYPE = parent.GENOTYPE
-ORDER BY child.ID
+WHERE
+    child.genotype & parent.genotype = parent.genotype
+ORDER BY id
