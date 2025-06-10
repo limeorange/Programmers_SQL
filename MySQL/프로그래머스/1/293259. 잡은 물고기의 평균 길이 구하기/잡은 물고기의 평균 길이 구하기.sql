8a586ebf654ef49f33ad557bbad95222f8999f76
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-06-10 22:36:13 +0900

    [level 1] Title: 잡은 물고기의 평균 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
+++ MySQL/프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
@@ -1,12 +1,5 @@
-# 250403 목 AM 12:27 / IF(조건, A, B)
-
-WITH f AS(
-    SELECT
-        ID,
-        IF(LENGTH is null, 10, LENGTH) AS LENGTH
-    FROM FISH_INFO
-)
+# 250610 화 PM 10:35 / IF(조건, A, B)
 
 SELECT
-    ROUND(AVG(LENGTH), 2) AS AVERAGE_LENGTH
-FROM f
+    ROUND(SUM(IF(length is null, 10, length))/COUNT(*), 2) AS average_length 
+FROM fish_info
