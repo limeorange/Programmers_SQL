8e667a09cb5e44f895c22083f5dc053b0bbd3b2a
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-06-11 16:58:31 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,18 +1,4 @@
-# 250404 금 PM 9:29
-
-WITH ap_table AS (
-    SELECT
-        apnt_no,
-        pt_no,
-        mcdp_cd,
-        mddr_id,
-        apnt_ymd
-    FROM appointment
-    WHERE
-        apnt_ymd LIKE '2022-04-13%' AND
-        apnt_cncl_yn = 'N' AND
-        mcdp_cd = 'CS'
-)
+# 250611 수 PM 4:58
 
 SELECT
     apnt_no,
@@ -20,10 +6,12 @@ SELECT
     a.pt_no,
     a.mcdp_cd,
     dr_name,
-    apnt_ymd
-FROM ap_table as a
-JOIN patient as p
-ON a.pt_no = p.pt_no
-JOIN doctor as d
+    apnt_ymd    
+FROM appointment AS a
+JOIN doctor AS d
 ON a.mddr_id = d.dr_id
-ORDER BY apnt_ymd
+JOIN patient AS p
+ON a.pt_no = p.pt_no
+WHERE apnt_cncl_yn = 'N' AND a.mcdp_cd = 'CS' AND
+      apnt_ymd LIKE '2022-04-13%'
+ORDER BY apnt_ymd 
