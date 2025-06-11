28326be6d7fc4a6e6d412a843538bd7ed6c1153c
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-06-11 16:49:12 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,48 +1,41 @@
-# 250404 금 PM 9:05
-# 오답포인트) 날짜 차이 계산시 '-' 사용하면 부정확 (단순 숫자 계산)
-# => DATEDIFF(end_date, start_date) 활용!
+# 250611 수 PM 4:49
 
-WITH 
-    his_table AS (
+WITH
+    truck_list AS (
     SELECT
-        history_id,
-        h.car_id,
-        daily_fee,
-        car_type,
-        DATEDIFF(end_date, start_date)+1 AS days
-    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
-    JOIN CAR_RENTAL_COMPANY_CAR as c
-    ON h.car_id = c.car_id
+        car_id, daily_fee
+    FROM car_rental_company_car
     WHERE car_type = '트럭'
     ),
-    
-    discount AS (
+    truck_discount_rate AS (
     SELECT
-        duration_type,
-        discount_rate
+        duration_type, discount_rate
     FROM car_rental_company_discount_plan
     WHERE car_type = '트럭'
     ),
-    
-    fee_table AS (
+    his_table AS (
     SELECT
         history_id,
         daily_fee,
-        days,
+        DATEDIFF(end_date, start_date)+1 AS day
+    FROM car_rental_company_rental_history AS a
+    JOIN truck_list AS b
+    ON a.car_id = b.car_id
+    ),
+    fee_table AS (
+    SELECT
+        *,
         CASE
-            WHEN (7 <= days AND days < 30) THEN 
-            (SELECT discount_rate FROM discount WHERE duration_type = '7일 이상')
-            WHEN (30 <= days AND days < 90) THEN 
-            (SELECT discount_rate FROM discount WHERE duration_type = '30일 이상')
-            WHEN (90 <= days) THEN 
-            (SELECT discount_rate FROM discount WHERE duration_type = '90일 이상')
+            WHEN day >= 90 THEN (SELECT discount_rate FROM truck_discount_rate WHERE duration_type = '90일 이상')
+            WHEN day >= 30 THEN (SELECT discount_rate FROM truck_discount_rate WHERE duration_type = '30일 이상')
+            WHEN day >= 7 THEN (SELECT discount_rate FROM truck_discount_rate WHERE duration_type = '7일 이상')
             ELSE 0
-        END AS discount
+        END AS discount_rate
     FROM his_table
     )
 
 SELECT
-    HISTORY_ID,
-    ROUND(daily_fee * days * (1-0.01*discount)) AS FEE
+    history_id,
+    ROUND(day * (1-discount_rate*0.01) * daily_fee) AS fee
 FROM fee_table
 ORDER BY fee DESC, history_id DESC
