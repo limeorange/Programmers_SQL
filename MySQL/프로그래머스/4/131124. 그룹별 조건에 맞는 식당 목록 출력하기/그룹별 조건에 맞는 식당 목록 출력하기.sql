16f0a1ea1401639ea161c4c072a236b7e3059515
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-06-11 16:15:20 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ MySQL/프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,28 +1,35 @@
-# 250403 목 PM 7:02
+# 250611 수 PM 4:15
 
-WITH
-    max_cnt AS(
-    SELECT
-        MEMBER_ID,
-        COUNT(*) AS CNT
-    FROM REST_REVIEW
-    GROUP BY MEMBER_ID
-    ORDER BY CNT DESC
+WITH 
+    cnt_table AS (
+        SELECT
+            member_id,
+            COUNT(*) AS cnt
+        FROM rest_review
+        GROUP BY member_id
+        ORDER BY cnt DESC
     ),
     
-    max_member_id AS (
-    SELECT
-        member_id
-    FROM max_cnt
-    WHERE cnt = (SELECT cnt FROM max_cnt LIMIT 1)
+    max_cnt AS (
+        SELECT
+            MAX(cnt) AS max_cnt
+        FROM cnt_table
+    ),
+    
+    member_list AS (
+        SELECT
+            member_id
+        FROM rest_review
+        GROUP BY member_id
+        HAVING COUNT(*) = (SELECT max_cnt FROM max_cnt)
     )
 
 SELECT
     member_name,
     review_text,
     DATE_FORMAT(review_date, '%Y-%m-%d') AS review_date
-FROM REST_REVIEW AS r
-JOIN MEMBER_PROFILE AS p
-ON r.member_id = p.member_id
-WHERE r.member_id in (SELECT member_id FROM max_member_id)
+FROM rest_review AS r
+JOIN member_profile AS m
+ON r.member_id = m.member_id
+WHERE r.member_id in (SELECT member_id FROM member_list)
 ORDER BY review_date, review_text
