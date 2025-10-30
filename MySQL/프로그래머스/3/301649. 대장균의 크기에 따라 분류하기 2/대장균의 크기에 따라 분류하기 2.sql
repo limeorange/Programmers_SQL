c672f562ea6929137d3738182d2318cedbcda022
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-10-30 20:56:46 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,24 +1,22 @@
-# 250610 화 PM 2:26
+# 251030 목 PM 8:56
 
-/*
-PERCENT_RANK() OVER (ORDER BY 컬럼명 정렬순서) AS 새로운_컬럼명
-=> 새로운_컬럼명에서 예약어(rank, order 등) 안 쓰도록 주의!
-*/
+# PERCENT_RANK() OVER (ORDER BY 컬럼명 정렬기준) AS 새로운_컬럼명
+# => 새로운_컬럼명에서 예약어(rank, order 등) 안 쓰도록 주의!
 
-WITH rank_info AS (
+WITH t1 AS (
     SELECT
-        *,
-        PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS rank_p
+        ID,
+        PERCENT_RANK() OVER (ORDER BY size_of_colony DESC) AS rank_p
     FROM ecoli_data
 )
 
 SELECT
-    ID,
+    id,
     CASE
         WHEN rank_p <= 0.25 THEN 'CRITICAL'
         WHEN rank_p <= 0.5 THEN 'HIGH'
         WHEN rank_p <= 0.75 THEN 'MEDIUM'
         ELSE 'LOW'
-    END AS COLONY_NAME
-FROM rank_info
-ORDER BY ID
+    END AS colony_name
+FROM t1
+ORDER BY id
