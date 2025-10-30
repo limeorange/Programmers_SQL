2342a0816ade618c27b188e477d04a00c1b3c346
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-10-30 21:18:10 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ MySQL/프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,9 +1,11 @@
-# 250610 화 PM 10:20
+# 251030 목 PM 9:17
 
-WITH max_info AS (
-    SELECT
+# (fish_type, max_length) 기준으로 해당 물고기 'ID'를 출력해야 함.
+
+WITH t1 AS (
+    SELECT 
         fish_type,
-        max(length) AS length
+        MAX(LENGTH) AS length
     FROM fish_info
     GROUP BY fish_type
 )
@@ -12,8 +14,8 @@ SELECT
     id,
     fish_name,
     length
-FROM fish_info AS f
-JOIN fish_name_info AS n
-ON f.fish_type = n.fish_type
-WHERE (f.fish_type, f.length) in (SELECT fish_type, length FROM max_info)
+FROM fish_info AS j1
+JOIN fish_name_info AS j2
+ON j1.fish_type = j2.fish_type
+WHERE (j1.fish_type, length) in (SELECT fish_type, length FROM t1)
 ORDER BY id
