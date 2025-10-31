e48be67b342cd7c0d680c9a83ec9a5b79056a8c7
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-10-31 20:04:11 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ MySQL/프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,18 +1,18 @@
-# 250611 수 AM 9:25
+# 251031 금 PM 8:03
 
-/*
-1) 8~10월 누적 5회 이상 차량만 필터링
-2) 누적 기준 필터 후 월별 집계
-오답포인트) 월별 집계에 대해 '5회 이상' 기준 적용하고 있었음.
-*/
+# 오답포인트) 월별 5회 이상이 아니라 8~10월 합해서 5회 이상임.
+# 오답포인트) t1의 car_id 조건을 외부 테이블에서 이용할 때 8~10월 날짜 조건도 다시 걸어줘야 함.
+# 1) 8~10월 동안 5회 이상인 car_list 구하기 => CTE
+# 2) 해당 car_list에 대해 월별 자동차 ID별 대여 횟수 구하기
 
-WITH car_table AS (
+WITH t1 AS (
     SELECT
-        car_id
+        car_id,
+        COUNT(*) AS records
     FROM car_rental_company_rental_history
-    WHERE start_date LIKE '2022-08%' OR
-          start_date LIKE '2022-09%' OR
-          start_date LIKE '2022-10%'
+    WHERE
+        '2022-08-01' <= start_date AND
+        start_date <= '2022-10-31'
     GROUP BY car_id
     HAVING COUNT(*) >= 5
 )
@@ -22,9 +22,8 @@ SELECT
     car_id,
     COUNT(*) AS records
 FROM car_rental_company_rental_history
-WHERE car_id in (SELECT car_id FROM car_table) AND
-      (start_date LIKE '2022-08%' OR
-      start_date LIKE '2022-09%' OR
-      start_date LIKE '2022-10%')
+WHERE car_id in (SELECT car_id FROM t1) AND
+    '2022-08-01' <= start_date AND
+    start_date <= '2022-10-31'
 GROUP BY month, car_id
 ORDER BY month, car_id DESC
