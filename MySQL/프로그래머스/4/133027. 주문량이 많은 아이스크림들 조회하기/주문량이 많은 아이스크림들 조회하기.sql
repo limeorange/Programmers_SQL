94ac407bf916a87f62da4f1b9e59f0368b69487e
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-10-31 20:50:50 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ MySQL/프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,18 +1,19 @@
-# 250611 수 PM 2:44
+# 251031 금 PM 8:50
 
-WITH total AS (
+# july 테이블은 '같은 맛이라도 서로 다른 출하 번호' => 자체적으로 group by 먼저해서 맛 별 total_order 구한 다음 first_half랑 join해야 전체적인 total_order 계산이 정확함.
+
+WITH j AS (
     SELECT
-        *
-    FROM first_half
-    UNION ALL
-    SELECT
-        *
+        flavor,
+        SUM(total_order) AS total_order
     FROM july
+    GROUP BY flavor
 )
 
 SELECT
-    flavor
-FROM total
-GROUP BY flavor
-ORDER BY SUM(total_order) DESC
+    a.flavor
+FROM first_half AS a
+JOIN j AS b
+ON a.flavor = b.flavor
+ORDER BY (a.total_order+b.total_order) DESC
 LIMIT 3
