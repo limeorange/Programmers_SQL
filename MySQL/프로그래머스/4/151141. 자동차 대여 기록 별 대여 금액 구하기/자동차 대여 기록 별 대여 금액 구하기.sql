2acc3a4a5942a8623a88b23c37f057972b31065f
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-10-31 21:46:59 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,41 +1,39 @@
-# 250611 수 PM 4:49
+# 251031 금 PM 9:46
 
-WITH
-    truck_list AS (
-    SELECT
-        car_id, daily_fee
-    FROM car_rental_company_car
-    WHERE car_type = '트럭'
-    ),
-    truck_discount_rate AS (
-    SELECT
-        duration_type, discount_rate
-    FROM car_rental_company_discount_plan
-    WHERE car_type = '트럭'
-    ),
-    his_table AS (
+WITH t1 AS (
     SELECT
         history_id,
-        daily_fee,
-        DATEDIFF(end_date, start_date)+1 AS day
-    FROM car_rental_company_rental_history AS a
-    JOIN truck_list AS b
-    ON a.car_id = b.car_id
-    ),
-    fee_table AS (
+        h.car_id,
+        start_date,
+        end_date,
+        DATEDIFF(end_date, start_date)+1 AS duration,
+        daily_fee
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY AS h
+    LEFT JOIN CAR_RENTAL_COMPANY_CAR AS c
+    ON h.car_id = c.car_id
+    WHERE car_type = '트럭'
+),
+truck_plan AS (
+    SELECT
+        *
+    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    WHERE
+        car_type = '트럭'
+),
+t2 AS (
     SELECT
         *,
         CASE
-            WHEN day >= 90 THEN (SELECT discount_rate FROM truck_discount_rate WHERE duration_type = '90일 이상')
-            WHEN day >= 30 THEN (SELECT discount_rate FROM truck_discount_rate WHERE duration_type = '30일 이상')
-            WHEN day >= 7 THEN (SELECT discount_rate FROM truck_discount_rate WHERE duration_type = '7일 이상')
+            WHEN duration >= 90 THEN (SELECT discount_rate FROM truck_plan WHERE duration_type = '90일 이상')
+            WHEN duration >= 30 THEN (SELECT discount_rate FROM truck_plan WHERE duration_type = '30일 이상')
+            WHEN duration >= 7 THEN (SELECT discount_rate FROM truck_plan WHERE duration_type = '7일 이상')
             ELSE 0
         END AS discount_rate
-    FROM his_table
-    )
+    FROM t1
+)
 
 SELECT
     history_id,
-    ROUND(day * (1-discount_rate*0.01) * daily_fee) AS fee
-FROM fee_table
+    ROUND(daily_fee * duration * (1-0.01*discount_rate)) AS fee
+FROM t2
 ORDER BY fee DESC, history_id DESC
