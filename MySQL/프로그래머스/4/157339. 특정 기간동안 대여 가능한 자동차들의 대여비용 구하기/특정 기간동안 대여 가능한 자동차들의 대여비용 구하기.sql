67f000f3af8e847fa43298b527cdb2de85c6bb6f
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-10-31 22:30:19 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ MySQL/프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,51 +1,53 @@
-# 250611 수 PM 3:53
+# 251031 금 PM 10:30
 
-/*
-car_rental_company_car: (대여 중인 = 전체) 자동차 정보
-car_rental_company_rental_history: 자동차 대여 기록 정보
-car_rental_company_discount_plan: 자동차 종류 별 대여 기간 종류 별 할인 정책 정보
+# 1) 22/11/1 ~ 22/11/30까지 대여 불가능한 차량 구해서 ('세단', 'SUV') 차량 목록에서 빼주기 => 대여 가능한 '세단' or 'SUV' 차량 구함.
+# 하나의 car_id에 대해 여러 his가 존재하기 때문에 불가능한 경우가 1가지라도 존재하면 그 차량은 대여가 불가능한 것임.
 
-오답포인트) "대여 이력이 없는" 차량도 고려해야 함.
-오답포인트) HAVING는 group by나 집계가 없다면 사용 X. CTE 결과를 where로 걸기.
-오답포인트) 대여 기록 정보 상으로 '불가능'한 car_id 구해서 not in으로 구하기!
-- car_id에 대해서 한 history_id라도 불가능한 경우가 발견되면 대여가 불가능한 것임.
-오답포인트) discount_rate 예시보고 하드코딩하면 안되고, 동적으로 변수화해서 넣어줘야 함. 
-*/
-
-WITH 
-    not_car_list AS (
+WITH car_not_list AS (
     SELECT
-        DISTINCT car_id
-    FROM car_rental_company_rental_history
-    WHERE start_date <= '2022-11-30' AND
-          end_date >= '2022-11-01'
-    ),
-        
-    discount_rate AS (
-    SELECT car_type, discount_rate
-    FROM car_rental_company_discount_plan
-    WHERE duration_type = '30일 이상'
-    ),
-        
-    car_list AS (
+        h.car_id
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY AS h
+    JOIN CAR_RENTAL_COMPANY_CAR AS c
+    ON h.car_id = c.car_id
+    WHERE
+        car_type in ('세단', 'SUV') AND
+        ('2022-11-01' <= start_date AND start_date <= '2022-11-30') OR
+        ('2022-11-01' <= end_date AND end_date <= '2022-11-30') OR
+        (start_date < '2022-11-01' AND '2022-11-30' < end_date)
+),
+discount_list AS (
+SELECT
+    car_type,
+    discount_rate
+FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+WHERE
+    car_type in ('세단', 'SUV') AND
+    duration_type = '30일 이상'
+),
+t1 AS (
     SELECT
-        c.car_id,
-        c.car_type,
+        car_id,
+        car_type,
+        daily_fee,
         CASE
-            WHEN c.car_type = '세단' THEN ROUND(daily_fee * (1-0.01*discount_rate) * 30)
-            WHEN c.car_type = 'SUV' THEN ROUND(daily_fee * (1-0.01*discount_rate) * 30)
-        END AS fee
-    FROM car_rental_company_car AS c
-    JOIN discount_rate AS d
-    ON c.car_type = d.car_type
-    WHERE c.car_type in ('세단', 'SUV') AND
-          car_id not in (SELECT car_id FROM not_car_list)
-    )
-    
+            WHEN car_type = '세단' 
+            THEN (SELECT discount_rate FROM discount_list WHERE car_type = '세단')
+            WHEN car_type = 'SUV' 
+            THEN (SELECT discount_rate FROM discount_list WHERE car_type = 'SUV')
+            ELSE 0
+        END AS discount_rate
+    FROM CAR_RENTAL_COMPANY_CAR
+    WHERE
+        car_type in ('세단', 'SUV') AND
+        car_id not in (SELECT car_id FROM car_not_list)
+)
+
 SELECT
     car_id,
     car_type,
-    fee
-FROM car_list
-WHERE 500000 <= fee AND fee < 2000000
+    ROUND(daily_fee * 30 * (1-0.01*discount_rate)) AS fee
+FROM t1
+WHERE
+    500000 <= daily_fee * 30 * (1-0.01*discount_rate) AND
+    daily_fee * 30 * (1-0.01*discount_rate) < 2000000
 ORDER BY fee DESC, car_type, car_id DESC
